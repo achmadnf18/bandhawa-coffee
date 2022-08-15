673e56f3d35a461c2b029665696f66782d824ed1
zidane917 <zidane917@gmail.com>
2022-08-15 09:23:42 +0700

    Update

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import jwt_decode from "jwt-decode";
 
 export default function dashboard() {
   const [datas, setDatas] = useState([]);
--- src/service/index.js
+++ src/service/index.js
@@ -1,15 +1,15 @@
-import axios from "axios";
-import { useRouter } from "next/router";
+// import axios from "axios";
+// import { useRouter } from "next/router";
 
-const URL = "http://localhost/3001/";
-const router = useRouter;
-export const LoginUser = async (data) => {
-  try {
-    await axios.post(`${URL}login`, data);
-    router.push("/");
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const URL = "http://localhost/3001/";
+// const router = useRouter;
+// export const LoginUser = async (data) => {
+//   try {
+//     await axios.post(`${URL}login`, data);
+//     router.push("/");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-export const GetUser = async (data) => {};
+// export const GetUser = async (data) => {};
