5e1d80fd0b3ebe7a273ca92b324feab24f6fcc19
Rizki Maulana  Jiddan <zidane917@gmail.com>
2022-12-22 22:59:35 +0700

    update : navigasi mobile

--- src/component/Navbar/navigation-item-mobile.js
+++ src/component/Navbar/navigation-item-mobile.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import React from "react";
+import { Fragment, useState } from "react";
+import { MobileDropMenu } from "./mobile-drop-menu";
 
 const navList = [
   {
@@ -23,16 +25,28 @@ const navList = [
   },
 ];
 export default function NavigationItemMobile() {
+  const [isOpenDropDown, setIsOpenDropDown] = useState(false);
   return (
     <div className="px-4 mt-4">
       <ul className="font-medium text-sm pt-3 px-3">
         {navList.map((list, index) => {
           return (
-            <li key={index + 1} className="py-2">
-              <Link href={`${list.to}`} passHref>
-                <a className="text-black hover:text-gray-500">{list.title}</a>
-              </Link>
-            </li>
+            <Fragment key={`navigasi-${index}`}>
+              {list?.id !== 3 ? (
+                <li key={index + 1} className="py-2">
+                  <Link href={`${list.to}`} passHref>
+                    <a className="text-black hover:text-gray-500">
+                      {list.title}
+                    </a>
+                  </Link>
+                </li>
+              ) : (
+                <MobileDropMenu
+                  isOpenDropDown={isOpenDropDown}
+                  setIsOpenDropDown={setIsOpenDropDown}
+                />
+              )}
+            </Fragment>
           );
         })}
       </ul>
